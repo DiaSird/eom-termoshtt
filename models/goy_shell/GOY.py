39611849ec2f88fd3fcaf7125b69f4cb523953ea
DiaSird <78157340+DiaSird@users.noreply.github.com>
2024-04-07 16:18:27 +0900

    feat:

--- models/goy_shell/GOY.py
+++ models/goy_shell/GOY.py
@@ -1,40 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# In[2]:
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# In[3]:
-
-
-get_ipython().system(' cargo run --release --example=goy_shell > goy.csv')
-
-
-# In[4]:
-
-
 df = pd.read_csv("goy.csv").set_index("time")
 
-
-# In[5]:
-
-
 df[["r4", "r8", "r16", "r26"]].plot()
-
-
-# In[6]:
-
+plt.savefig("GOY.png")
+plt.clf()
 
 (df**2).mean().plot()
 plt.yscale("log")
-
+plt.savefig("error.png")
